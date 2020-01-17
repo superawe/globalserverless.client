d32ff3874caf61a4e78f2bb54ac2787e73a40f48
Arvid Sahlin <arvid@pario.se>
2020-01-17 15:33:46 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
+
+const Home = () => <h1>Home</h1>;
+const Admin = () => <h1>Admin</h1>;
+const NavBar = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/admin">Admin</Link>
+    </li>
+  </ul>
+);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Router>
+        <NavBar />
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/">
+              <Redirect to="/"></Redirect>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
